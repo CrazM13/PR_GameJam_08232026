11bd1585c42aae178559248b846c76401a2ccdbd
CrazM13 <sepulvedajoey13@yahoo.com>
2026-08-23 12:35:36 -0400

    Added player prefab

---
+++ GameJam/Scripts/Character/Character.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public partial class Character : CharacterBody3D {
+
+	public override void _PhysicsProcess(double delta) {
+		base._PhysicsProcess(delta);
+
+		if (!this.IsOnFloor()) {
+			this.Velocity += this.GetGravity() * 3f * (float) delta;
+		}
+
+		this.MoveAndSlide();
+
+		this.Velocity = new Vector3(this.Velocity.X * 0.7f, this.Velocity.Y, this.Velocity.Z * 0.7f);
+	}
+
+	public void Move(Vector3 direction) {
+		this.Velocity += direction;
+	}
+
+	public void AttemptJump() {
+		if (this.IsOnFloor()) {
+			this.Velocity += this.GetGravity() * -1f;
+		}
+	}
+
+}
---
+++ GameJam/Scripts/Character/PlayerController.cs
@@ -0,0 +1,43 @@
+using Godot;
+using System;
+
+public partial class PlayerController : Node {
+
+	[Export] private Character body;
+	[Export] private Camera3D camera;
+
+	[Export] private float speed = 1;
+
+	public override void _Ready() {
+		base._Ready();
+
+		Input.MouseMode = Input.MouseModeEnum.Captured;
+
+	}
+
+	public override void _Process(double delta) {
+		base._Process(delta);
+
+		if (Input.IsActionPressed("move_jump")) {
+			body.AttemptJump();
+		}
+
+		Vector3 forward = body.Transform.Basis.X;
+		Vector3 right = body.Transform.Basis.Z;
+
+		Vector3 movement = (Input.GetAxis("move_left", "move_right") * forward) + (Input.GetAxis("move_forward", "move_backward") * right);
+
+		body.Move(movement.Normalized() * speed);
+
+		Vector2 mouseVel = Input.GetLastMouseVelocity() * (float)delta;
+		body.RotateY(Mathf.DegToRad(-mouseVel.X));
+		camera.Rotation = new Vector3(Mathf.Clamp(camera.Rotation.X + Mathf.DegToRad(-mouseVel.Y * 0.25f), -Mathf.Pi * 0.5f, Mathf.Pi * 0.5f), 0, 0);
+
+		if (Input.MouseMode == Input.MouseModeEnum.Captured && Input.IsActionJustPressed("ui_cancel")) {
+			Input.MouseMode = Input.MouseModeEnum.Visible;
+		} else if (Input.MouseMode == Input.MouseModeEnum.Visible && Input.IsMouseButtonPressed(MouseButton.Left)) {
+			Input.MouseMode = Input.MouseModeEnum.Captured;
+		}
+	}
+
+}
